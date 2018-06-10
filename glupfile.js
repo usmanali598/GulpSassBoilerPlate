542e2e2cc9ae58b88e3ae80b34f3019589175e3a
usman <usman_pervaiz@live.com>
2018-06-10 18:10:51 +0300

    new branch & gulp tasks added

--- glupfile.js
+++ glupfile.js
@@ -1,3 +1,11 @@
 const gulp = require( 'gulp' );
 const browserSync = require( 'browser-sync' ).create();
 const sass = require( 'gulp-sass' );
+
+gulp.task( 'sass', function ()
+{
+    return gulp.src( [ 'src/scss/*.scss' ] )
+        .pipe( sass() )
+        .pipe( gulp.dest( 'src/css' ) )
+        .pipe( browserSync.stream() );
+} );
