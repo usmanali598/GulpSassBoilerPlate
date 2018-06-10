9095468e5582ee677d0ddf9e1b8198a1db1212f2
usman <usman_pervaiz@live.com>
2018-06-10 18:31:01 +0300

    live-server running & html added

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,15 @@ gulp.task( 'sass', function ()
         .pipe( gulp.dest( 'src/css' ) )
         .pipe( browserSync.stream() );
 } );
+
+gulp.task( 'serve', [ 'sass' ], function ()
+{
+    browserSync.init( {
+        server: './src'
+    } );
+
+    gulp.watch( [ 'src/scss/*.scss' ], [ 'sass' ] );
+    gulp.watch( [ 'src/*.html' ] ).on( 'change', browserSync.reload );
+} );
+
+gulp.task( 'default', [ 'serve' ] );
